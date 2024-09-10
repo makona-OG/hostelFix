dd1d773572cad4e9701b9e51b1f2fecf607e8a4b
Pharel Makona <pharelmakona@gmail.com>
2024-09-10 21:59:52 +0300

    hostelFix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, session
+from flask import Flask, render_template,methods, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
